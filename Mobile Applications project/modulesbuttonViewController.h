becb34d6d01c5ceb089bbed9df1844ec7c46d846
Chukwuebuka Nwibe <Nwibe@chukwuebukas-macbook-air.local>
2017-12-08 14:12:54 +0000

    modules page started

---
+++ Mobile Applications project/modulesbuttonViewController.h
@@ -0,0 +1,15 @@
+//
+//  modulesbuttonViewController.h
+//  Mobile Applications project
+//
+//  Created by Chukwuebuka Nwibe on 08/12/2017.
+//  Copyright © 2017 Chukwuebuka Nwibe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface modulesbuttonViewController : UIViewController
+
+- (IBAction)viewaddbuttonPressed:(id)sender;
+
+@end
---
+++ Mobile Applications project/modulesbuttonViewController.m
@@ -0,0 +1,41 @@
+//
+//  modulesbuttonViewController.m
+//  Mobile Applications project
+//
+//  Created by Chukwuebuka Nwibe on 08/12/2017.
+//  Copyright © 2017 Chukwuebuka Nwibe. All rights reserved.
+//
+
+#import "modulesbuttonViewController.h"
+
+@interface modulesbuttonViewController ()
+
+@end
+
+@implementation modulesbuttonViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)viewaddbuttonPressed:(id)sender {
+
+    
+}
+@end
